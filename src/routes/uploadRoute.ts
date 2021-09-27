62b0196624257984429534c47931b1112a46b8f1
Kaan <hizir8899@gmail.com>
2021-09-27 11:40:09 +0300

    Update fileUploadMiddlewarr

--- src/routes/uploadRoute.ts
+++ src/routes/uploadRoute.ts
@@ -1,11 +1,10 @@
 /* istanbul ignore file */
 import express from 'express';
-import { UPLOAD_FILE_FIELD } from '../constants';
 import { uploadFileController } from '../controllers/uploadFileController';
-import uploadFile from '../helpers/uploadFile';
+import { fileUploadMiddleWare } from '../helpers/uploadFile';
 
 const uploadRoute = express.Router();
 
-uploadRoute.post('/', uploadFile.single(UPLOAD_FILE_FIELD), uploadFileController);
+uploadRoute.post('/', fileUploadMiddleWare, uploadFileController);
 
 export default uploadRoute;
