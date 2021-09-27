c40479acd4ea73ae88db60677b1da7d07661ed8f
Kaan <hizir8899@gmail.com>
2021-09-27 11:51:10 +0300

    Fix resize issue

--- src/controllers/serveFileController.ts
+++ src/controllers/serveFileController.ts
@@ -1,10 +1,10 @@
 import { getFileResized } from '../helpers/getResizedFile';
 
-export function serveFileControllerWithResize(req, res) {
+export async function serveFileControllerWithResize(req, res) {
   const { fileName } = req.params;
   const { resize } = req.query;
   const resizeWidth = Number(resize) || undefined;
 
-  const { buffer, fileExtension } = getFileResized(fileName, resizeWidth);
+  const { buffer, fileExtension } = await getFileResized(fileName, resizeWidth);
   return res.setHeader('Content-Type', `image/${fileExtension}`).send(buffer);
 }
