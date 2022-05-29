314f06ce77c6d5a1264584604a652ccafb429803
kaanersoy <kaan.ersoy@trendyol.com>
2022-05-29 14:46:56 +0300

    add getResizedFile helper tests

--- tests/getResizedFile.spec.ts
+++ tests/getResizedFile.spec.ts
@@ -1,10 +1,31 @@
 import * as path from "path";
 import * as fs from "fs";
-import * as sharp from "sharp";
 import { getFileResized } from "../src/helpers/getResizedFile";
 
-describe("getFileResized specs", () => {
-  it("should call function succesfully", () => {
+jest.mock("fs");
+jest.mock("path");
+jest.mock("sharp", () => () => ({
+  resize: jest.fn().mockReturnThis(),
+  toBuffer: jest.fn(() => "someBuffer"),
+}));
 
+describe("getFileResized specs", () => {
+  const fileExtension = "yes";
+  const fileName = `someStr.${fileExtension}`;
+  const resizeWidth = 180;
+
+  const buffer = "someBuffer";
+
+  it("should call function succesfully", async () => {
+    (fs.readFileSync as jest.Mock).mockReturnValue(buffer);
+    (path.join as jest.Mock).mockReturnValue("");
+
+    const expectedBuffer = "someBuffer";
+    const result = await getFileResized(fileName, resizeWidth);
+
+    expect(result).toStrictEqual({
+      buffer: expectedBuffer,
+      fileExtension,
+    });
   });
 });
